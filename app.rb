46cd7120c0a62224aa69be93c5940db48d348029
Shawn Anderson <shawn42@gmail.com>
2014-07-14 03:39:57 -0400

    water at 9am, using a new thread to not block data collection while watering

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ include Clockwork
 garden = Garden.new GARDEN_LATITUDE, GARDEN_LONGITUDE
 puts "Found your garden at #{GARDEN_LATITUDE}, #{GARDEN_LONGITUDE}"
 
-every(1.day, 'water_if_needed_daily', at: '13:46') do
+every(1.day, 'water_if_needed_daily', at: '9:00', thread: true) do
   garden.auto_water
 end
 every(5.minutes, 'record_readings') do
